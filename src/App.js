ed7f5f03fbb2d3321fb96dd0bc89d7ca473d9693
srivarshiniexe-byte <srivarshini.exe@gmail.com>
2026-01-02 14:22:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import SearchIcon from "./search.svg";
 
 //f577b867
-const API_URL = "http://www.omdbapi.com?apikey=f577b867";
+const API_URL = "https://www.omdbapi.com?apikey=f577b867";
 
 const movie1 = {
   Poster: "N/A",
